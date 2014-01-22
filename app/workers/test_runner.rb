2bd3783a24ba3a95d9e1133886cecfbb7c006762
Dan Franko <dfranko@gmail.com>
2014-01-22 12:19:02 -0500

    Change runner

--- app/workers/test_runner.rb
+++ app/workers/test_runner.rb
@@ -6,26 +6,5 @@ class TestRunner
     
     Dir.chdir("/Users/admin/Documents/Development/trial2")
     `rspec`
-    
-    # require 'watir-webdriver'
-    # b = Watir::Browser.new :chrome
-    # b.goto("yale.edu")
-#     
-    # require 'gmail'
-    # g = Gmail.new("yaleqa@gmail.com","qcrm2013")
-#     
-    # text = b.text
-    # g.deliver do
-      # to "dan.franko@yale.edu"
-      # subject "Having fun in Puerto Rico!"
-      # text_part do
-        # body @text
-      # end
-      # html_part do
-        # content_type 'text/html; charset=UTF-8'
-        # body text
-      # end
-      # b.close
-    # end
   end
 end
