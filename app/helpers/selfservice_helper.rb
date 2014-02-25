b3b80ff4dd93aa81ab2cb409ca2dadae88573a56
Dan Franko <dfranko@gmail.com>
2014-02-24 20:19:25 -0500

    Add ignore

--- app/helpers/selfservice_helper.rb
+++ app/helpers/selfservice_helper.rb
@@ -1,2 +1,15 @@
 module SelfserviceHelper
+  def user?
+    User.table_exists? && User
+  end
+  
+  def user
+    if user?
+      if  u = User.find_by_netid(session[:cas_user])
+        u.name
+      else
+        session[:cas_user]
+      end
+    end
+  end
 end
