7d0ee2fbedaa49fd2d9e67663f426f9e5838330a
Dan Franko <dfranko@gmail.com>
2015-02-22 15:33:38 -0500

    Remove about page and fix names of fixtures

--- test/functional/applications_controller_test.rb
+++ test/functional/applications_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
-class AppsControllerTest < ActionController::TestCase
+class ApplicationsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
   end
-
 end
--- test/models/application_user_permission_test.rb
+++ test/models/application_user_permission_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AppUserPermissionTest < ActiveSupport::TestCase
+class ApplicaitonUserPermissionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
